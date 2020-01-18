718ecdf725a26aa42072514ad2f6a4333961a4c4
unknown <caherryannnepomuceno28@gmail.com>
2020-01-18 11:52:03 +0800

    OKAY NA

---
+++ ajax.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#btn-table").click(function(){
+        $("#container").load("table.html");
+
+    });
+});
+
+
+$(document).ready(function(){
+    $("#btn-dialog").click(function(){
+        $("#container").load("dialog.html");
+
+    });
+});
+
